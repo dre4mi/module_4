b7f1608daccdf1c780878c5dc4424c0ccd03938b
Андрей <andreyka.istomin.04@gmail.com>
2023-06-29 21:56:22 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# def strcounter(string):
+#     for symbol in string:
+#         counter = 0
+#         for sub_symbol in string:
+#             if symbol == sub_symbol:
+#                 counter += 1
+#             print(symbol, counter)
+# strcounter('aavvbde')
+
+def strcounter(string):
+    for symbol in set(string):
+        counter = 0
+        for sub_symbol in string:
+            if symbol == sub_symbol:
+                counter += 1
+            print(symbol, counter)
+strcounter('aavvbde')
+D:\pycharm projects\pythonProject3
